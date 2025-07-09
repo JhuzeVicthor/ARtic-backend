148129241960ff046211ea50556fa1b8a08c4479
JhuzeVicthor <jhuzevicthor63@gmail.com>
2025-07-08 22:11:08 -0300

    commit inicial

---
+++ src/main/java/com/ARtic/ARtic/ARticApplication.java
@@ -0,0 +1,13 @@
+package com.ARtic.ARtic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ARticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ARtic/ARtic/ARticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ARtic.ARtic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ARticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
